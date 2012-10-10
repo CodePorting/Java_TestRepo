75c1cd683abae1314968dfd62c9b37efc33f8b56
CodePorting <info@codeporting.com>
2012-10-10 03:11:40 -0700

    Auto port from CodePorting

---
+++ PortedCode/CodePorting-Ported/App.xaml.java
@@ -0,0 +1,16 @@
+package WpfApplication2;
+
+// ********* THIS FILE IS AUTO PORTED FROM C# USING CODEPORTING.COM *********
+
+
+
+/**
+     * <p>
+     * Interaction logic for App.xaml or not.
+     * </p>
+     */
+public partial class App extends Application
+{
+	private int iTestVariable;
+}
+
