89793f6da093a830425b06842bc2114896f3bf77
CodePorting <info@codeporting.com>
2012-10-10 03:11:49 -0700

    Auto port from CodePorting

---
+++ PortedCode/CodePorting-Ported/AssemblyInfo.java
@@ -0,0 +1,48 @@
+// ********* THIS FILE IS AUTO PORTED FROM C# USING CODEPORTING.COM *********
+
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+assembly:/*Attribute target specifier not applicable to java*/ @AssemblyTitleAttribute ("WpfApplication2")
+assembly:/*Attribute target specifier not applicable to java*/ @AssemblyDescriptionAttribute ("")
+assembly:/*Attribute target specifier not applicable to java*/ @AssemblyConfigurationAttribute ("")
+assembly:/*Attribute target specifier not applicable to java*/ @AssemblyCompanyAttribute ("")
+assembly:/*Attribute target specifier not applicable to java*/ @AssemblyProductAttribute ("WpfApplication2")
+assembly:/*Attribute target specifier not applicable to java*/ @AssemblyCopyrightAttribute ("Copyright ?  2009")
+assembly:/*Attribute target specifier not applicable to java*/ @AssemblyTrademarkAttribute ("")
+assembly:/*Attribute target specifier not applicable to java*/ @AssemblyCultureAttribute ("")
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+assembly:/*Attribute target specifier not applicable to java*/
+
+//In order to begin building localizable applications, set 
+//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
+//inside a <PropertyGroup>.  For example, if you are using US english
+//in your source files, set the <UICulture> to en-US.  Then uncomment
+//the NeutralResourceLanguage attribute below.  Update the "en-US" in
+//the line below to match the UICulture setting in the project file.
+
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+
+assembly:/*Attribute target specifier not applicable to java*/ @ThemeInfo (ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+    //(used if a resource is not found in the page, 
+    // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly)
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision...  updated
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+assembly:/*Attribute target specifier not applicable to java*/ @AssemblyVersionAttribute ("1.0.0.0")
+assembly:/*Attribute target specifier not applicable to java*/ @AssemblyFileVersionAttribute ("1.0.0.0")
